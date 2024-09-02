f2d8d168a8c5a7319911e0882a602452369bcfc9
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-09-02 10:12:36 -0300

    Commit weeks 9 & 10

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const dataLine1 = {
     labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
     datasets: [{
-      label: 'Km recorridos en Semana 7',
-      data: [18, 13, 11, 17], // Datos de los km recorridos en cada día
+      label: 'Km recorridos en Semana 9',
+      data: [15, 12, 20, 28], // Datos de los km recorridos en cada día
       borderColor: 'rgba(75, 192, 192, 1)',
       fill: true,
       tension: 0.1
@@ -16,8 +16,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const dataLine2 = {
     labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
     datasets: [{
-      label: 'Km recorridos en Semana 8',
-      data: [14, 15, 9, 20], // Datos de los km recorridos en cada día
+      label: 'Km recorridos en Semana 10',
+      data: [14, 17, 22, 26], // Datos de los km recorridos en cada día
       borderColor: 'rgba(153, 102, 255, 1)',
       fill: true,
       tension: 0.1
@@ -25,40 +25,32 @@ document.addEventListener("DOMContentLoaded", function () {
   };
 
   // Configuración del gráfico de líneas
-  const configLine = {
-    type: 'line',
-    data: dataLine1, // Puede cambiar esto a dataLine2 para mostrar el gráfico de la segunda sección
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
+// Seleccione los contextos de los <canvas> y cree los gráficos
+const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
+  type: 'line',
+  data: dataLine1, // Puede cambiar esto a dataLine2 para mostrar el gráfico de la segunda sección
+  options: {
+  scales: {
+  y: {
+     beginAtZero: true
     }
-  };
-
-  // Seleccione los contextos de los <canvas> y cree los gráficos
-  const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), configLine);
+   }
+  }
+  });
+  
   const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
-    type: 'line',
-    data: dataLine2,
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
+  type: 'line',
+  data: dataLine2,
+  options: {
+  scales: {
+      y: {
+      beginAtZero: true
       }
+     }
     }
   });
-  function onOpen() {
-    var ui = SpreadsheetApp.getUi();
-    ui.createMenu('Menu Personalizado')
-         .addItem('Agregar Marca de Agua', 'addWatermark')
-         .addToUi();
-   }
-   
-
-function actualizarDiasRestantes() {
+  
+  function actualizarDiasRestantes() {
     // Obtener el título que contiene la fecha objetivo
     const pageTitle =  document.getElementById('pageTitle').innerText;
       
@@ -79,41 +71,41 @@ function actualizarDiasRestantes() {
     resultElement.textContent = `Faltan ${daysDiff} días para llegar a la carrera.`;
     console.log("actualizarDiasRestantes function called");
     }
-
+  
     // Crear dos nuevas instancias de HTMLVideoElement para cada sección
     let Semana_1_video_1 = document.createElement("video");
     let Semana_1_video_2 = document.createElement("video");
     let Semana_2_video_3 = document.createElement("video");
     let Semana_2_video_4 = document.createElement("video");
-
+  
     // Establecer las fuentes de video
     Semana_1_video_1.src = "video 1.mp4"; // ruta de su primer video
     Semana_1_video_2.src = "video 2.mp4"; // ruta de su segundo video
     Semana_2_video_3.src = "video 3.mp4"; // ruta de su tercer video
     Semana_2_video_4.src = "video 4.mp4"; // ruta de su cuarto video
-
+  
     // Establecer atributos de video
     Semana_1_video_1.controls = true;
     Semana_1_video_1.autoplay = false;
     Semana_1_video_1.loop = false;
-
+  
     Semana_1_video_2.controls = true;
     Semana_1_video_2.autoplay = false;
     Semana_1_video_2.loop = false;
-
+  
     Semana_2_video_3.controls = true;
     Semana_2_video_3.autoplay = false;
     Semana_2_video_3.loop = false;
-
+  
     Semana_2_video_4.controls = true;
     Semana_2_video_4.autoplay = false;
     Semana_2_video_4.loop = false;
-
+  
     // Agregar los dos videos a cada sección
     document.getElementById("Semana 1").appendChild(video1);
     document.getElementById("Semana 1").appendChild(video2);
     document.getElementById("Semana 2").appendChild(video3);
     document.getElementById("Semana 2").appendChild(video4);
-
-});
-
+  
+  });
+  
